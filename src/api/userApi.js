6477a17fa23a63529a3dfdb3fe22c302a76fb214
Ewald van Veen <ewaldvanveen@gmail.com>
2022-01-16 15:53:44 +0100

    Added Delete function to the http mock class

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,8 +7,20 @@ export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(userId) {
+  return del(`users/${userId}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Dummy code to check if everything is working from the start.
 import './index.css';
 import numeral from 'numeral';
-import { getUsers } from './api/userApi';
+import { deleteUser, getUsers } from './api/userApi';
 
 const boilerplateValue = numeral(1000).format('$0,0.00');
 
@@ -24,4 +24,20 @@ getUsers().then((result) => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use 'Array.from' to create a real array from a DOM-collection.
+  // 'getElementsByClassName' only creates an 'array like' object.
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+
+      deleteUser(element.attributes['data-id'].value);
+
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
 });
